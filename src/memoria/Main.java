0c92ccbbcb023e06d59f4cf793613f06eabb53f3
Miguel0310 <miguel95.mn@gmail.com>
2018-10-19 20:03:22 -0300

    Initial commit

---
+++ src/memoria/Main.java
@@ -0,0 +1,32 @@
+package memoria;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static String [] memoria = new String[]{"","A","","","","","B","B","B",""};
+    public static int opcao = 0;
+
+    public static void main(String args[]){
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Opcao: ");
+        opcao = ler.nextInt();
+        boolean flag = true;
+        controlMemoria cm = new controlMemoria();
+        while (flag == true) {
+            switch (opcao) {
+                case 1:
+                    cm.opciones(1,2,2); //Pasa por parametro los espacios de la memoria que se van a usar.
+                    opcao = ler.nextInt();
+                    break;
+                case 2:
+                    opcao = ler.nextInt();
+                    break;
+                default:
+                    flag = false;
+                    System.out.println("Saiu");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/memoria/controlMemoria.java
@@ -0,0 +1,108 @@
+package memoria;
+/*Caso 1:
+* 10 locaciones en total
+* Programa +  Espacio
+*    C     +     1
+*    D     +     2
+*    E     +     2   */
+public class controlMemoria {
+    Main mn = new Main();
+
+    public void spacenullBF(int num,String letra) { //Codigo para encontrar los espacios libres en Best Fit.
+        int contador = 0, box;
+        boolean achei = true, flag = false;
+        for (int i = 0; i < 10; i++) {
+            if (mn.memoria[i].isEmpty()) {
+                contador++;
+            } else {
+                if (!mn.memoria[i].isEmpty()) {
+                    box = contador;
+                    contador = 0;
+                    if (box == num) {
+                        achei = false;
+                        for (int j = 0; j < num; j++) {
+                            mn.memoria[(i - 1) - j] = letra;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        if (achei) {
+            flag = true;
+            contador = 0;
+            while (flag) {
+                flag = false;
+                for (int p = 0; p < 10; p++) {
+                    if (mn.memoria[p].isEmpty()) {
+                        contador++;
+                        if (contador >= num) {
+                            contador = 0;
+                            flag = false;
+                            for (int x = 0; x < num; x++) {
+                                mn.memoria[p - x] = letra;
+                            }
+                            break;
+                        }
+                    } else if (!mn.memoria[p].isEmpty()) {
+                        contador = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public void spacenullWF(int num, String letra) {//Codigo para encontrar los espacios libres en Worst Fit.
+        int contador = 0, box = 0, indice = 0;
+        //boolean flag = false;
+        for (int i = 0; i < 10; i++) {
+            if ((mn.memoria[i].isEmpty()) && (i != 9)) {
+                contador++;
+            } else if ((!mn.memoria[i].isEmpty())) {
+                if (i == 9) {
+                    contador++;
+                }
+                if (contador >= num) {
+                    if (contador >= box) {
+                        box = contador;
+                        indice = i - box;
+                    }
+                }
+                contador = 0;
+            }
+        }
+        for (int j = 0; j < num; j++) {
+            mn.memoria[indice + j] = letra;
+        }
+        indice = 0;
+    }
+
+
+
+
+    public void opciones(int C,int D,int E){ //caso1
+
+        System.out.println("BestFit");
+        spacenullBF(C, "C");
+        spacenullBF(D, "D");
+        spacenullBF(E, "E");
+        for(int i=0;i<10;i++) {
+            System.out.print("|" + mn.memoria[i]);
+        }
+        mn.memoria = new String[]{"", "A", "", "", "", "", "B", "B", "B", ""};
+
+        System.out.println("\nWorstFit");
+        spacenullWF(C,"C");
+        spacenullWF(D,"D");
+        spacenullWF(E,"E");
+        for(int i=0;i<10;i++) {
+            System.out.print("|" + mn.memoria[i]);
+        }
+        mn.memoria = new String[]{"", "A", "", "", "", "", "B", "B", "B", ""};
+
+    }
+
+    public void BestFit(int C,int D,int E,int F){//demás casos
+
+    }
+}
